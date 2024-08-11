ffc7d2fbc85d114c07c6daae1f02db2d40b3b36a
jackhasakeyboard <jackhasakeyboard@gmail.com>
2024-08-11 05:48:07 -0600

    Output name of subreddit prefixed by the thing designating it as a subreddit

--- main.c
+++ main.c
@@ -1,3 +1,27 @@
+#include <stdio.h>
+
+const char prefix = 'r';
+
+const char sep = '/';
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc == 1) {
+		err("No arguments");
+
+		return 1;
+	}
+
+	if (argc != 1 + 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("%c%c%s", prefix, sep, argv[1]);
+
 	return 0;
 }
